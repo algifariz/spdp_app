a86553418985f87efd63cc6ab1dde6c81e4ddacb
Ikram Maulana <ikram075@ummi.ac.id>
2023-09-20 20:47:42 +0700

    Chore: Add Alert to Dashboard
    Add:
    - Alert pemberitahuan jika masih ada guru yang belum memiliki jam mengajar

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Models\Jabatan;
+use App\Models\JamMengajar;
 use App\Models\Tunjangan;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
@@ -16,11 +18,21 @@ class DashboardController extends Controller
     $data = null;
 
     if (Auth::user()->hasRole('admin')) {
+      $user = User::all();
+      $jamMengajar = JamMengajar::all();
+      $availableGuruDoesntHaveJamMengajar = $user->filter(function ($item) use ($jamMengajar) {
+        return !$jamMengajar->pluck('nuptk')->contains($item->nuptk) && !$item->hasRole('admin');
+      });
+
       $data = (object) [
         'metrics_guru' => Role::where('name', 'guru')->first()->users()->count() ?? 0,
         'metrics_jabatan' => Jabatan::count() ?? 0,
         'metrics_tunjangan' => Tunjangan::count() ?? 0,
       ];
+
+      if ($availableGuruDoesntHaveJamMengajar->count() > 0) {
+        $data->availableGuruDoesntHaveJamMengajar = $availableGuruDoesntHaveJamMengajar->count();
+      }
     } else {
       $data = (object) [
         'metrics_qr' => 0,
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -4,6 +4,28 @@
             <h1 class="block text-2xl font-bold text-gray-800 sm:text-3xl dark:text-white">Dashboard</h1>
         </header>
 
+        <div class="p-4 border border-yellow-200 rounded-md bg-yellow-50" role="alert">
+            <div class="flex">
+                <div class="flex-shrink-0">
+                    <svg class="h-4 w-4 text-yellow-400 mt-0.5" xmlns="http://www.w3.org/2000/svg" width="16"
+                        height="16" fill="currentColor" viewBox="0 0 16 16">
+                        <path
+                            d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z" />
+                    </svg>
+                </div>
+                <div class="ml-4">
+                    <h3 class="text-sm font-semibold text-yellow-800">
+                        Pemberitahuan
+                    </h3>
+                    <div class="mt-1 text-sm text-yellow-700">
+                        Masih terdapat {{ $data->availableGuruDoesntHaveJamMengajar }} guru yang belum memiliki jam
+                        mengajar, silahkan tambahkan jam mengajar <a href="{{ route('admin.jam-mengajar.index') }}"
+                            class="text-yellow-700 underline">di sini</a>.
+                    </div>
+                </div>
+            </div>
+        </div>
+
         <div class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-7">
             @if (Auth::user()->hasRole('admin'))
                 <div
@@ -12,9 +34,9 @@
                         <div class="flex items-center gap-4">
                             <div
                                 class="flex items-center justify-center w-20 h-20 bg-blue-600 rounded-lg aspect-square text-zinc-50">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
-                                    fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
-                                    stroke-linejoin="round" class="lucide lucide-users">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+                                    viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
+                                    stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-users">
                                     <path d="M16 21v-2a4 4 0 0 0-4-4H6a4 4 0 0 0-4 4v2" />
                                     <circle cx="9" cy="7" r="4" />
                                     <path d="M22 21v-2a4 4 0 0 0-3-3.87" />
@@ -92,8 +114,10 @@
                                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
                                     viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
                                     stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-qr-code">
-                                    <rect width="5" height="5" x="3" y="3" rx="1" />
-                                    <rect width="5" height="5" x="16" y="3" rx="1" />
+                                    <rect width="5" height="5" x="3" y="3"
+                                        rx="1" />
+                                    <rect width="5" height="5" x="16" y="3"
+                                        rx="1" />
                                     <rect width="5" height="5" x="3" y="16"
                                         rx="1" />
                                     <path d="M21 16h-3a2 2 0 0 0-2 2v3" />
