44dc2c3a33f34e375850adea4fc2a071c17a13c0
Ikram Maulana <ikram075@ummi.ac.id>
2023-09-20 19:07:14 +0700

    Chore: Update Detail Guru

--- resources/views/dashboard/admin/guru/show.blade.php
+++ resources/views/dashboard/admin/guru/show.blade.php
@@ -19,7 +19,6 @@
         <div
             class="flex flex-col bg-white border shadow-sm rounded-xl p-4 md:p-5 dark:bg-gray-800 dark:border-gray-700 dark:shadow-slate-700/[.7] dark:text-gray-400">
             <div class="flex flex-col gap-4">
-                {{-- name, nuptk, tempat_lahir, tanggal_lahir, jenis_kelamin, agama, alamat, no_hp, nama_jabatan, nama_tunjangan --}}
                 <div class="form-group">
                     <label for="input-label" class="block mb-2 text-sm font-medium dark:text-white">Nama Guru</label>
                     <input type="text" name="name"
